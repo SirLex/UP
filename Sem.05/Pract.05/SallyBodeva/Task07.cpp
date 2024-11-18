19ca9c03a3bb84b4d66413e61977fd44f86ce949
Salihe Bodeva <salihe.bodeva10@gmail.com>
2024-11-18 08:21:25 +0200

    Update Task07.cpp

--- Sem.05/Pract.05/SallyBodeva/Task07.cpp
+++ Sem.05/Pract.05/SallyBodeva/Task07.cpp
@@ -1,54 +1,68 @@
-// Task07.cpp : This file contains the 'main' function. Program execution begins and ends there.
+Ôªø// Task07.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
 using namespace std;
 
-int longestSequenceOf≈qualNums(int arr[],int size ) {
+const int MATRIX_SIZE = 3;
 
-	int currentSequence = 1;
-	int longestSequence = 1;
+void sumOfMatrixes(int matrix1[][MATRIX_SIZE], int matrix2[][MATRIX_SIZE], int result[][MATRIX_SIZE]) {
 
-	for (int i = 0; i < size-1; i++)
+	for (int i = 0; i < MATRIX_SIZE; i++)
 	{
-		if (arr[i] == arr[i+1])
+		for (int j = 0; j < MATRIX_SIZE; j++)
 		{
-			currentSequence++;
-		}
-		else
-		{
-			if (currentSequence > longestSequence) {
-				longestSequence = currentSequence;
-			}
-
-			currentSequence = 1;
+			result[i][j] = matrix1[i][j] + matrix2[i][j];
 		}
 	}
-	if (currentSequence > longestSequence)
-	{
-		longestSequence = currentSequence;
-	}
-	return longestSequence;
+
 }
 
-void readArray(int arr[], int size) {
+void multiply(int matrix1[][MATRIX_SIZE], int result[][MATRIX_SIZE], int multiplyer) {
 
-	cout << "Enter " << size << " elements: ";
-	for (int i = 0; i < size; i++) {
-		cin >> arr[i];
+	for (int i = 0; i < MATRIX_SIZE; i++)
+	{
+		for (int j = 0; j < MATRIX_SIZE; j++)
+		{
+			result[i][j] = matrix1[i][j] * multiplyer;
+		}
+	}
+
+}
+
+void printMatrix(int matrix[MATRIX_SIZE][MATRIX_SIZE]) {
+
+	for (int i = 0; i < MATRIX_SIZE; i++)
+	{
+		for (int j = 0; j < MATRIX_SIZE; j++)
+		{
+			cout << matrix[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+}
+void readMatrix(int matrix[MATRIX_SIZE][MATRIX_SIZE])
+{
+	for (int i = 0; i < MATRIX_SIZE; i++)
+	{
+		for (int j = 0; j < MATRIX_SIZE; j++)
+		{
+			cin >> matrix[i][j];
+		}
 	}
 }
 
 int main()
 {
-	int arr[100] = {};
+	int matrix[MATRIX_SIZE][MATRIX_SIZE];
+	int multiplyer;
+	cin >> multiplyer;
+	int result[MATRIX_SIZE][MATRIX_SIZE];
 
-	cout << "Enter the size of the array: ";
+	readMatrix(matrix);
 
-	int size;
-	cin >> size;
+	multiply(matrix, result, multiplyer);
 
-	readArray(arr, size);
-
-	cout << "The longest sequence of equal nums has length of " << longestSequenceOf≈qualNums(arr, size);
+	printMatrix(result);
 }
