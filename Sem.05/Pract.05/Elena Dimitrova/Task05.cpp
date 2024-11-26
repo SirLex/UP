7d622bdf3d0e96674167f1054008e2b69124c006
EliDimi <2275eli@gmail.com>
2024-11-26 10:41:21 +0200

    Update Task05.cpp

--- Sem.05/Pract.05/Elena Dimitrova/Task05.cpp
+++ Sem.05/Pract.05/Elena Dimitrova/Task05.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
 using namespace std;
 
-const int SIZE = 100;
+const int SIZE = 1000;
 
-int isSymetric(int arr[], int size) {
+void readArray(int arr[], int size) {
+	cout << "Enter array: ";
 	for (int i = 0; i < size; i++) {
+		cin >> arr[i];
+	}
+}
+
+bool isSymetric(int arr[], int size) {
+	int halfSize = size / 2;
+	for (int i = 0; i < halfSize; i++) {
 		if (arr[i] != arr[size - i - 1]) {
 			return 0;
 		}
@@ -19,10 +27,7 @@ int main() {
 	cout << "Enter size of array: ";
 	cin >> size;
 
-	cout << "Enter array: ";
-	for (int i = 0; i < size; i++) {
-		cin >> arr[i];
-	}
+	readArray(arr, size);
 
 	if (isSymetric(arr, size)) {
 		cout << "yes";
@@ -30,4 +35,5 @@ int main() {
 	else {
 		cout << "no";
 	}
+	return 0;
 }
