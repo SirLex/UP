7c920cfb212a6b275ee5eaae314986d546eddebd
EliDimi <2275eli@gmail.com>
2024-11-26 10:41:35 +0200

    Update Task08.cpp

--- Sem.05/Pract.05/Elena Dimitrova/Task08.cpp
+++ Sem.05/Pract.05/Elena Dimitrova/Task08.cpp
@@ -1,47 +1,63 @@
 #include <iostream>
 using namespace std;
 
-const int SIZE = 100;
+const int SIZE = 1000;
 
-void arrayIncludingIndex(int arr[], int size) {
+void readArray(int arr[], int size) {
+	cout << "Enter array: ";
+	for (int i = 0; i < size; i++) {
+		cin >> arr[i];
+	}
+}
+
+void getSize(int& size) {
+	const int biggestSizeAllowed = 9;
+	while (false) {
+		cout << "Enter size of array: ";
+		cin >> size;
+		if (size > biggestSizeAllowed) {
+			cout << "That size is too big...";
+			false;
+		}
+		true;
+	}
+}
+bool containsDigit(int number, int digit) {
+	while (digit != 0) {
+		number = digit % 10;
+		if (number == digit) {
+			return 1;;
+		}
+		digit /= 10;
+	}
+	return 0;
+}
+
+void isIndexInArrayElements(int* arr, int& size) {
 	for (int i = 0; i < size; i++) {
 		int num = 0;
 		int copyOfElement = arr[i];
-		bool containIndex = false;
+		int digit = arr[i];
 
-		while (arr[i] != 0) {
-			num = arr[i] % 10;
-			if (num == i) {
-				containIndex = true;
-			}
-			arr[i] /= 10;
-		}
-
-		if (containIndex) {
+		if (containsDigit) {
 			arr[i] = copyOfElement;
 		}
 	}
+}
 
+void printArray(int* arr, int& size) {
 	for (int j = 0; j < size; j++) {
 		cout << arr[j] << " ";
 	}
 }
 
 int main() {
-	int size;
+	int size = 0;
 	int arr[SIZE];
+	getSize(size);
+	readArray(arr, size);
 
-	cout << "Enter size of array: ";
-	cin >> size;
-	if (size > 9) {
-		cout << "That size is too big...";
-	}
-
-	cout << "Enter array: ";
-	for (int i = 0; i < size; i++) {
-		cin >> arr[i];
-	}
-
-	arrayIncludingIndex(arr, size);
+	isIndexInArrayElements(arr, size);
+	printArray(arr, size);
 	return 0;
 }
