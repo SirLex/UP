7ef789616626d0fd226261ad76b0266251dd6be7
Mihail <107298750+MihailDimitrov1235@users.noreply.github.com>
2024-12-11 10:13:20 +0200

    Create Task07.cpp

---
+++ Sem.08/Student's Solutions/Mihail Dimitrov/Task07.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+
+const int MAX_SIZE = 1024;
+
+bool isWordContained(char* symbols, char* word, int symbolsLen) {
+    if (symbols == nullptr)
+    {
+        return false;
+    }
+    if (word == nullptr)
+    {
+        return false;
+    }
+
+    int counter = 0;
+
+    while (*word != '\0') {
+        counter++;
+        if (counter > symbolsLen)
+        {
+            return false;
+        }
+        if (*word != *symbols)
+        {
+            return false;
+        }
+        word++;
+        symbols--;
+    }
+
+    return true;
+}
+
+bool doExist(char* symbols, char* word) {
+    if (symbols == nullptr)
+    {
+        return 0;
+    }
+    if (word == nullptr)
+    {
+        return 0;
+    }
+
+    int counter = 0;
+    while (*symbols != '\0') {
+        counter++;
+        symbols++;
+    }
+    symbols--;
+    while (counter > 0) {
+        if (isWordContained(symbols, word, counter)) {
+            return true;
+        }
+        symbols--;
+        counter--;
+    }
+    return false;
+}
+
+int main()
+{
+    char symbols[MAX_SIZE] = "1test1";
+    char word[MAX_SIZE] = "tset";
+    cout << doExist(symbols, word);
+}
