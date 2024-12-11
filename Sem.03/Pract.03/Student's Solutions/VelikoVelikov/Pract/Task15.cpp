8d7381395c5f5adc407b6004a5772b1e3d92b37d
bul-bbx <kratos20050129@gmail.com>
2024-12-11 19:28:27 +0200

    Create Task15.cpp

---
+++ Sem.03/Pract.03/Student's Solutions/VelikoVelikov/Pract/Task15.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number; cin >> number;
+
+    if (number <= 0) {
+        cout << "Invalid input";
+        return 0;
+    }
+
+
+    bool first = true;
+
+    int maxbitsize = 30;
+
+    for (int i = maxbitsize; i >= 0; i--) {
+        int powerOfTwo = 1 << i;
+        if (number >= powerOfTwo) {
+            if (!first) {
+                cout << " + ";
+            }
+            cout << "2^" << i;
+            number -= powerOfTwo;
+            first = false;
+        }
+    }
+}
