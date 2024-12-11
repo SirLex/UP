e0ea8e4a9c4b35e898b6b081e20de7189e87aac6
bul-bbx <kratos20050129@gmail.com>
2024-12-11 19:28:22 +0200

    Create Task12.cpp

---
+++ Sem.03/Pract.03/Student's Solutions/VelikoVelikov/Pract/Task12.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number;
+    cin >> number;
+
+    int temp = number, reversed = 0;
+    int len = 0;
+
+    while (temp > 0) {
+        reversed = reversed * 10 + temp % 10;
+        temp /= 10;
+        len++;
+    }
+
+    int result = 0, factor = 1;
+    int middle = len / 2;
+    temp = reversed;
+
+    for (int i = 0; i < len; i++) {
+        if (!(len % 2 == 1 && i == middle) && !(len % 2 == 0 && (i == middle || i == middle - 1))) {
+            result = result * 10 + temp % 10;
+        }
+        temp /= 10;
+    }
+
+    cout << result << ", ";
+    result++;
+    cout << result;
+}
