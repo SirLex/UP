060a77e7cad61430ab4a6bc1109f3c84766036c0
bul-bbx <kratos20050129@gmail.com>
2024-12-11 19:28:20 +0200

    Create Task11.cpp

---
+++ Sem.03/Pract.03/Student's Solutions/VelikoVelikov/Pract/Task11.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n; cin >> n;
+    if (n <= 3)
+    {
+        return 0;
+    }
+    int prev = 1;
+    int temp = 1;
+    cout << 1 << " ";
+    for (int i = 1; i <= n; i += prev)
+    {
+        prev = temp;
+        cout << i << " ";
+        temp = i;
+    }
+}
