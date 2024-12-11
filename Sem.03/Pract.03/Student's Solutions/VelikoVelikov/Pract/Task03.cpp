5ef732e2368256ba6e760921ac1e8bbcf507c9d8
bul-bbx <kratos20050129@gmail.com>
2024-12-11 19:28:02 +0200

    Create Task03.cpp

---
+++ Sem.03/Pract.03/Student's Solutions/VelikoVelikov/Pract/Task03.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n; cin >> n;
+
+    int square = n * n;
+    int result = 0;
+
+    while (square != 0)
+    {
+        result = result * 10 + square % 10;
+        square /= 10;
+    }
+    cout << result;
+}
