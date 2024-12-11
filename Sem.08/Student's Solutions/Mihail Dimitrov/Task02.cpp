759e5cbe08e3ea6c0e2a42de0aab3514499db5b0
Mihail <107298750+MihailDimitrov1235@users.noreply.github.com>
2024-12-11 22:22:58 +0200

    fixed task 2

--- Sem.08/Student's Solutions/Mihail Dimitrov/Task02.cpp
+++ Sem.08/Student's Solutions/Mihail Dimitrov/Task02.cpp
@@ -3,54 +3,53 @@ using namespace std;
 
 const int MAX_SIZE = 1024;
 
-int countSymbols(char arr[], char symbol) {
-    int counter = 0;
-    while (*arr != '\0') {
-        if (*arr == symbol)
-        {
-            counter++;
-            *arr = ' ';
-        }
-        arr++;
-    }
-    return counter;
-}
+int CheckStr(char arr[])
+{
 
-int CheckStr(char arr[]) {
+    const int ASCII_TABLE_SIZE = 255;
+    const int MINIMUM_SYMBOLS_REQUIRED = 2;
 
     if (arr == nullptr)
     {
-        return -1;   
+        return -1;
     }
-    int oddSymbols = 0;
+
+    int symbolCounterArray[ASCII_TABLE_SIZE] = {0};
+
     int counter = 0;
-    while (*arr != '\0') {
+    while (*arr != '\0')
+    {
 
-        if (*arr == ' ')
-        {
-            arr++;
-            counter++;
-            continue;
-        }
-
-        if (countSymbols(arr, *arr) % 2 == 1)
-        {
-            oddSymbols++;
-        }
-
+        symbolCounterArray[*arr]++;
         counter++;
         arr++;
     }
 
-    if (counter < 2)
+    if (counter < MINIMUM_SYMBOLS_REQUIRED)
     {
         return -1;
     }
+
+    int oddSymbols = 0;
+
+    for (size_t i = 0; i < ASCII_TABLE_SIZE; i++)
+    {
+        if (symbolCounterArray[i] != 0)
+        {
+            cout << symbolCounterArray[i] << " " << (char)i << endl;
+        }
+        if (symbolCounterArray[i] % 2 == 1)
+        {
+            oddSymbols++;
+        }
+    }
+
     if (oddSymbols == 0 && counter % 2 == 0)
     {
         return 1;
     }
-    else if (oddSymbols == 1 && counter % 2 == 1) {
+    else if (oddSymbols == 1 && counter % 2 == 1)
+    {
         return 1;
     }
     return 0;
@@ -58,6 +57,6 @@ int CheckStr(char arr[]) {
 
 int main()
 {
-    char arr[MAX_SIZE] = "asdfgjk";
+    char arr[MAX_SIZE] = "dasdasf";
     cout << CheckStr(arr);
 }
