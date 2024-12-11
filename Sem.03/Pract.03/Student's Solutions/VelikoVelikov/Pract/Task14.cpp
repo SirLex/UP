8d10eca251056c6ebbfa2d2c3b99103d6d9a8027
bul-bbx <kratos20050129@gmail.com>
2024-12-11 19:28:25 +0200

    Create Task14.cpp

---
+++ Sem.03/Pract.03/Student's Solutions/VelikoVelikov/Pract/Task14.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n; cin >> n;
+	if (n == 0)
+		cout << false;
+	else
+	{
+		while (n != 0)
+		{
+			if (n % 2 == 1 && n > 1)
+			{
+				cout << false;
+				return 0;
+			}
+			n /= 2;
+		}
+		cout << true;
+	}
+}
